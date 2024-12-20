2780ffc0d3c441ff7a9500f924b970c1bad2118a
arsalanahmad123 <arsalanahmad15092003@gmail.com>
2024-12-20 11:49:22 +0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,72 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hello world</h1>
+    <button @click="start" :disabled="isPlaying">play</button>
+    <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+    <Results v-if="showResults" :score="score" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Block from './components/Block.vue';
+import Results from './components/Results.vue';
+
+
 
 export default {
   name: 'App',
+  data(){
+    return {
+        isPlaying: false,
+        delay: null,
+        score: null,
+        showResults: false
+    }
+  },
   components: {
-    HelloWorld
+    Block,
+    Results
+  },
+  methods: {
+    start(){
+        this.delay = 2000 + Math.random() * 5000
+        this.isPlaying = true 
+        this.showResults = false
+    },
+
+    endGame(reactionTime){
+        this.score = reactionTime;
+        this.isPlaying = false
+        this.showResults = true 
+    }
+
+
+
   }
+  
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+h1{
+    font-size: 30px;
+    color: rgb(16, 88, 88);
+}
+button{
+    padding: 5px 20px;
+    background: rgb(16, 88, 88);
+    color: white;
+    border-radius: 5px;
+    cursor: pointer;
+    border: none;
+}
+button[disabled]{
+    cursor: not-allowed;
+    opacity: 0.2;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{ sale: theme === 'sale' }" >
+            <slot></slot>
+        </div>
+    </div>    
+</template>
+
+<script>
+    export default {
+        name: 'Modal',
+        props: ['theme', ],
+        methods: {
+            closeModal(){
+                this.$emit('close')
+            }
+        }
+    }
+</script>
+
+
+<style scoped>
+    .modal{
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+    .backdrop{
+        top: 0;
+        position: fixed;
+        background: rgba(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100%;
+        align-content: center;
+    }
+    .modal.sale{
+        background: black;
+        color: white;
+    }
+    h1{
+        color: orangered;
+    }
+</style>
